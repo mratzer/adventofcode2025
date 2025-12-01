bfbb526bfaff7279398b03052be4bac02236d2a2
Markus Ratzer <markus.ratzer@bearingpoint.com>
2025-12-01 14:31:21 +0100

    Day 01 - Part 1

--- src/main/java/eu/marrat/adventofcode2025/day01/Day01.java
+++ src/main/java/eu/marrat/adventofcode2025/day01/Day01.java
@@ -1,9 +1,67 @@
 package eu.marrat.adventofcode2025.day01;
 
+import eu.marrat.adventofcode2025.util.Utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Day01 {
 
     static void main() {
         System.out.println("hello advent of code 2025");
+
+        Pattern pattern = Pattern.compile("([LR]{1})(\\d+)");
+
+        System.out.println("---");
+        int[] ints = Utils.getLines("day01/input.txt")
+                .map(pattern::matcher)
+                .filter(Matcher::matches)
+                .mapToInt(e -> {
+                    int multiplier = getMultiplier(e.group(1));
+                    int amount = Integer.parseInt(e.group(2));
+
+                    return amount * multiplier;
+                })
+                .toArray();
+
+        int count = 0;
+        int value = 50;
+
+        for (int i : ints) {
+            if (i < 0) {
+                System.out.format("%3d - %3d = %3d -> ", value, i * -1, value + i);
+            } else {
+                System.out.format("%3d + %3d = %3d -> ", value, i, value + i);
+            }
+
+            value += i;
+
+            while (value < 0) {
+                value = 100 + value;
+            }
+
+            while (value > 99) {
+                value = value - 100;
+            }
+
+            System.out.format("%3d%n", value);
+
+            if (value == 0) {
+                count++;
+            }
+        }
+
+
+        System.out.println(count);
+        System.out.println("---");
+    }
+
+    private static int getMultiplier(String direction) {
+        return switch (direction) {
+            case "R" -> 1;
+            case "L" -> -1;
+            case null, default -> throw new RuntimeException("Unknown direction: " + direction);
+        };
     }
 
 }
